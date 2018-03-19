8dddbe4fffa518b8067d474d8cd9d6537a68d32a
gefero <german.rosati@gmail.com>
2018-03-19 11:10:46 -0300

    Cambios en Script C4 - agrego ejercicio reglog

--- Scripts/C4_Starter_Script.R
+++ Scripts/C4_Starter_Script.R
@@ -7,7 +7,7 @@
 ##############################################################################
 
 ############################
-## ACTIVIDADES UNIDAD 2. ##
+## ACTIVIDADES UNIDAD 3. ##
 ############################
 
 ## 1. IMPORTAR EL DATASET "ED_deptos_venta_2014" 
@@ -27,3 +27,32 @@ data<-read.csv("ED_deptos_venta_2014.csv", sep=";")
 ## 5. SELECCIONE AQUELLAS VARIABLES QUE CONSIDERE RELEVANTES PARA LA DETERMINACION 
 ##    DEL PRECIO DEL DEPARTAMENTO Y GENERE UN MODELO DE REGRESION MULTIPLE. 
 ##    REALICE UNA EVALUACION DEL MISMO.
+
+
+
+## 6. IMPORTAR EL DATASET "data_filt.csv". 
+# Es una versión resumida de la EPH 2do. semestre de 2015. 
+# Contiene las siguientes variables de los asalariados ocupados:
+        # Edad -ch06-
+        # Nivel educativo -nivel_ed-
+        # Horas de trabajo totales -htot-
+        # Calificación de la ocupación -calif-
+        # Ingreso laboral total -p47t-
+# (se asume que el archivo está en el working directory)
+
+setwd("/media/grosati/Elements/PEN/KINGSTON/PEN2/Cursos/REPO_Curso_Fund_Prog_R/Data")
+df <- read.csv("data_filt.csv")
+
+## 7. REALICE UN ANÁLISIS EXPLORATORIO DE LOS DATOS: 
+# ¿QUÉ VARIABLES SE ENCUENTRAN ASOCIADAS?
+
+## 8. RECODIFICAR LA VARIABLE p47t en dos grupos (altos y bajos ingresos) A PARTIR DE LA MEDIANA
+
+## 9. ENTRENAR UN MODELO DE REGRESIÓN LOGISTICA PARA PREDECIR LA PROBABILIDAD DE QUE UNA PERSONA
+# OCUPADA Y ASALARIADA TENGA INGRESOS MAYORES A LA MEDIANA. 
+## Asegurarse de estimar de forma correcta el error.
+
+
+
+
+summary(df$p47t)
